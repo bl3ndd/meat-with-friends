a1bff954dc6c182cd4753120e96924af462685de
bl3ndd <evarzin@viasattech.ru>
2024-04-20 13:00:47 +0400

    add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import './App.css'
 
 function App() {
+    const friends = [
+        { name: 'Kolyan', daysBusy: ['mon', 'tue'] },
+        { name: 'Volodya', daysBusy: ['mon', 'tue'] },
+        { name: 'Zheka', daysBusy: ['mon', 'tue'] },
+        { name: 'Seryoga', daysBusy: ['mon', 'tue'] },
+    ]
     return (
         <>
             <div className="body">
-                <button className="button">Click</button>
+                <h1>Friends</h1>
+                <div className="friends">
+                    { friends.map((friend) => {
+                        return <div className="friendCard">
+                            { friend.name }:
+                            { friend.daysBusy.map((day) => <div>{ day }</div>) }
+                        </div>
+                    }) }
+                </div>
             </div>
         </>
     )
--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,6 @@ import './index.css'
 import WebApp from '@twa-dev/sdk'
 
 WebApp.ready();
-WebApp.setHeaderColor('bg_color')
-WebApp.setBackgroundColor('secondary_bg_color')
-
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
