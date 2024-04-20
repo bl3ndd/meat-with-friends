a0cb75bfe38a80aebb69d548d68bab9300e6afe4
bl3ndd <evarzin@viasattech.ru>
2024-04-20 11:52:06 +0400

    set bg

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
     return (
         <>
             <div className="body">
-                <button></button>
+                <button>Click</button>
             </div>
         </>
     )
