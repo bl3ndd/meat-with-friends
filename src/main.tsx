3c237bdde8ccc6e290d77ef197b5ec76e9115633
bl3ndd <evarzin@viasattech.ru>
2024-04-20 10:51:11 +0400

    add cdn twa

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-import WebApp from '@twa-dev/sdk'
+// import WebApp from '@twa-dev/sdk'
 
-WebApp.ready();
+window.Telegram.WebApp.ready();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
